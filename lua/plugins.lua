43b4b07ed28e38aacf4bb17f50b4c21a30aa3ba6
sergey <sergey@sergeys-MacBook-Pro.local>
2023-06-09 12:32:56 +0300

    added gitignore, added color scheme

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -6,4 +6,9 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
+
+  -- Color scheme
+  -- use 'folke/tokyonight.nvim'
+  use 'gruvbox-community/gruvbox'
+
 end)
