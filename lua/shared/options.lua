e02ef1e61075b8e35995ac8780ca5c65a6f29a96
sergey <sergey@sergeys-MacBook-Pro.local>
2023-06-09 12:08:38 +0300

    added more shared options

--- lua/shared/options.lua
+++ lua/shared/options.lua
@@ -4,4 +4,12 @@ vim.o.relativenumber = true
 
 -- identation
 vim.o.smartindent = true
--- vim.o.
+vim.o.smarttab = true
+-- TODO: check if it is required
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.softtabstop = 4
+
+-- misc
+vim.opt.swapfile = false
+vim.o.colorcolumn = 80
